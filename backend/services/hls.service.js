93627de042643fe35d268e563b49adf9a67fa8df
Prashantt6 <royprashant14@gmail.com>
2026-01-14 21:41:36 +0545

    HLS service introduced

---
+++ backend/services/hls.service.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+const { spawn } = require("child_process");
+const fs = require('fs')
+const path = require('path')
+
+const generateHLS = async (videoURL) =>{
+
+}
+
+module.exports = {generateHLS}
