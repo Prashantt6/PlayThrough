2dd55b05300ce6a18aa5660c3b2ca020fc86d609
Prashantt6 <royprashant14@gmail.com>
2026-01-15 13:37:45 +0545

    request and respond set for hls

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,15 @@ app.use(
 // Routes
 app.use('/api',streamRouter)
 app.use('/api', hlsRouter)
-
+app.use("/hls", (req, res, next) => {
+  if (req.url.endsWith(".m3u8")) {
+    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+  }
+  if (req.url.endsWith(".ts")) {
+    res.setHeader("Content-Type", "video/mp2t");
+  }
+  next();
+});
 app.use('/hls',express.static(path.join(__dirname, "hls")))
 
 app.get('/', (req,res) =>{
